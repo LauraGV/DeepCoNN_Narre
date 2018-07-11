59f6bfb96c7253f51155faf8e6624a827c1992ee
Avita <lauragilvelasco@hotmail.com>
2018-07-11 18:26:54 +0200

    import dill as pickle & otros cambios para cambiar version de python

--- PhD_180601_DeepCoNN_Narre/pro_data/loaddata.py
+++ PhD_180601_DeepCoNN_Narre/pro_data/loaddata.py
@@ -13,6 +13,7 @@ import json
 import pandas as pd
 import pickle
 import numpy as np
+import dill as pickle
 TPS_DIR = '../data/music'
 TP_file = os.path.join(TPS_DIR, 'Digital_Music_5.json')
 
@@ -26,10 +27,10 @@ np.random.seed(2017)
 for line in f:
     js=json.loads(line)
     if str(js['reviewerID'])=='unknown':
-        print "unknown"
+        print ("unknown")
         continue
     if str(js['asin'])=='unknown':
-        print "unknown2"
+        print ("unknown2")
         continue
     reviews.append(js['reviewText'])
     users_id.append(str(js['reviewerID'])+',')
@@ -89,13 +90,13 @@ item_reviews={}
 user_rid={}
 item_rid={}
 for i in data.values:
-    if user_reviews.has_key(i[0]):
+    if i[0] in user_reviews:
         user_reviews[i[0]].append(i[3])
         user_rid[i[0]].append(i[1])
     else:
         user_rid[i[0]]=[i[1]]
         user_reviews[i[0]]=[i[3]]
-    if item_reviews.has_key(i[1]):
+    if i[1] in user_reviews:
         item_reviews[i[1]].append(i[3])
         item_rid[i[1]].append(i[0])
     else:
@@ -104,18 +105,18 @@ for i in data.values:
 
 
 for i in data2.values:
-    if user_reviews.has_key(i[0]):
+    if i[0] in user_reviews:
         l=1
     else:
         user_rid[i[0]]=[0]
         user_reviews[i[0]]=['0']
-    if item_reviews.has_key(i[1]):
+    if i[1] in user_reviews:
         l=1
     else:
         item_reviews[i[1]] = [0]
         item_rid[i[1]]=['0']
 
-print item_reviews[11]
+#print item_reviews[11]
 pickle.dump(user_reviews, open(os.path.join(TPS_DIR, 'user_review'), 'wb'))
 pickle.dump(item_reviews, open(os.path.join(TPS_DIR, 'item_review'), 'wb'))
 pickle.dump(user_rid, open(os.path.join(TPS_DIR, 'user_rid'), 'wb'))
@@ -124,6 +125,6 @@ pickle.dump(item_rid, open(os.path.join(TPS_DIR, 'item_rid'), 'wb'))
 usercount, itemcount = get_count(data, 'user_id'), get_count(data, 'item_id')
 
 
-print np.sort(np.array(usercount.values))
+print (np.sort(np.array(usercount.values)))
 
-print np.sort(np.array(itemcount.values))
+print (np.sort(np.array(itemcount.values)))
